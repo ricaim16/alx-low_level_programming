7f85457fad169fa47ddf12dc295e2b3de6c4fa31
ricaim16 <kibatugidey0@gmail.com>
2023-04-25 16:52:31 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
 
 /**
- * main - Prints the add of the even-valued
- * fibonacci numbers.
+ *  main - Prints the first 98 Fibonacci numbers
  *
- * Return: Always 0.
+ *  Return: Always 0.
  */
 int main(void)
 {
-	long int n1, n2, fn, afn;
+	int c, boolean, boolean2;
+	long int n1, n2, fn, fn2, n11, n22;
 
 	n1 = 1;
 	n2 = 2;
-	fn = afn = 0;
-	while (fn <= 4000000)
+	boolean =  boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
-		fn = n1 + n2;
-		n1 = n2;
-		n2 = fn;
-		if ((n1 % 2) == 0)
+		if (boolean)
 		{
-			afn += n1;
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = fn;
 		}
+		else
+		{
+			if (boolean2)
+			{
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
+		}
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
 	}
-	printf("%ld\n", afn);
+	printf("\n");
 	return (0);
 }

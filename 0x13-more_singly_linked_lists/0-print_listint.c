27d8b216e0ed492a130b1ceab0f83a836d678872
ricaim16 <kibatugidey0@gmail.com>
2023-06-05 07:00:37 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
   * print_listint - Prints all the elements of a linked list
   * @h: The head of the linked list
   *
-  * Return: The number of nodes of the linked list
+  * Return: number of nodes of the linked list
   */
 size_t print_listint(const listint_t *h)
 {
